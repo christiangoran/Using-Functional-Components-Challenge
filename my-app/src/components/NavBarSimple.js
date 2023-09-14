c89af3592f12729b7771fae1b27fccc7b12520dd
christiangoran <christian.goran@protonmail.com>
2023-09-14 19:38:18 +0200

    Add NavBarSimple-component

---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,45 @@
+import React from "react";
+import css from "./css/NavBarSimple.module.css";
+
+class NavBarSimple extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Hello Guest!",
+      message: "Login",
+    };
+  }
+
+  handleClick() {
+    this.setState(
+      (prevState, prevProps) => {
+        return {
+          name:
+            prevState.name === "Hello Guest!"
+              ? "Welcome Back, user!"
+              : "Hello Guest!",
+          message: prevState.message === "Login" ? "Logout" : "Login",
+        };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className={css.NavBar}>
+        <h1>My Gallery</h1>
+        <div>
+          <span>{this.state.name}</span>
+          <button onClick={() => this.handleClick()}>
+            {this.state.message}
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NavBarSimple;
